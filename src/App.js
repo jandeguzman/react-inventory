c4a59530e212d3da08a7f6c215bcf0338830faf8
Sharareh Shafiei <sharareh@Shararehs-MacBook-Pro.local>
2018-02-03 13:33:04 -0800

    updating app and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
 import Inventory from './pages/Inventory';
 import Orders from './pages/Orders';
 import StockControl from './pages/stockControl';
+import Navbar from './components/navbar';
+
 
 const App = () => {
   <Router>
@@ -15,8 +18,7 @@ const App = () => {
         <Route exact path="/home/inventory" component={Inventory} />
         <Route exact path="/home/orders" component={Orders} />
         <Route exact path="/home/stockcontrol" component={StockControl} />
-        <Route exact path="/home/logout" component={Logout} />
-        <Route component={NoMatch} />
+        <Route exact path="/home/logout" component={Login} />
       </Switch>
     </div>
   </Router>;
