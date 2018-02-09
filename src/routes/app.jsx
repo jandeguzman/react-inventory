0610977902b55a9658ebfcbfdc1afffea8a3f3ac
mateyo3 <mateoreyes720@gmail.com>
2018-02-08 17:17:50 -0800

    sidebar nav button change

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -3,7 +3,6 @@ import UserProfile from "views/UserProfile/UserProfile.jsx";
 import Inventory from "views/Inventory/Inventory.jsx";
 import Orders from "views/Orders/Orders.jsx";
 import StockControl from "views/StockControl/StockControl.jsx";
-import Signup from "views/Signup/Signup.jsx";
 
 import {
     Dashboard, Person, ContentPaste
@@ -11,11 +10,10 @@ import {
 
 const appRoutes = [
     { path: "/dashboard", sidebarName: "Dashboard", navbarName: "Dashboard", icon: Dashboard, component: DashboardPage },
-    { path: "/user", sidebarName: "User Profile", navbarName: "Profile", icon: Person, component: UserProfile },
     { path: "/Inventory", sidebarName: "Inventory", navbarName: "Inventory", icon: ContentPaste, component: Inventory },
     { path: "/Orders", sidebarName: "Orders", navbarName: "Orders", icon: ContentPaste, component: Orders},
     { path: "/StockControl", sidebarName: "Stock Control", navbarName: "Stock Control", icon: ContentPaste, component: StockControl},
-    { path: "/signup", sidebarName: "Create User", navbarName: "Create User", icon: Person, component: Signup},
+    { path: "/user", sidebarName: "Create New User", navbarName: "Create New User", icon: Person, component: UserProfile },
 
     { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
 ];
