a11f531bb0bafcf3bf0219b6504cdcda995099dc
mateyo3 <mateoreyes720@gmail.com>
2018-02-08 20:59:26 -0800

    login page

---
+++ src/views/Login/Login.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+    Grid,
+} from 'material-ui';
+
+import {
+    ProfileCard, RegularCard, Button, CustomInput, ItemGrid
+} from 'components';
+
+
+class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            password:''
+        }
+    }
+    render(){
+        return (
+            <div>
+                <Grid container>
+                    <ItemGrid xs={12} sm={12} md={8}>
+                        <RegularCard
+                            headerColor="green"
+                            cardTitle="User Login"
+                            cardSubtitle=""
+                            content={
+                                <div>
+                                    <Grid container>
+                                        <ItemGrid xs={12} sm={12} md={6}>
+                                            <CustomInput
+                                                labelText="Username"
+                                                id="username"
+                                                onChange = {(event,newValue) => this.setState({username:newValue})}
+                                                formControlProps={{
+                                                    fullWidth: true
+                                                }}
+                                            />
+                                        </ItemGrid>
+                                        <ItemGrid xs={12} sm={12} md={6}>
+                                            <CustomInput
+                                                labelText="Password"
+                                                id="password"
+                                                onChange = {(event,newValue) => this.setState({password:newValue})}
+                                                formControlProps={{
+                                                    fullWidth: true
+                                                }}
+                                            />
+                                        </ItemGrid>
+                                    </Grid>
+                                </div>
+                            }
+                            footer={
+                                <Button color="info" onClick="">Login</Button>
+                            }
+                        />
+                    </ItemGrid>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default Login;
